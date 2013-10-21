b2d415d8e79603b92e9b2f49b50c460456ab9952
leon_huang <leon_huang@gemteks.com>
2013-10-21 18:04:13 +0800

    Send Bluetooth remote control command
    stop:  00100000 11100000
    up:    00100000 11010000
    down:  00100000 11110000
    left:  00010000 11100000
    right: 00110000 11100000

--- source/App.js
+++ source/App.js
@@ -1,17 +1,81 @@
 enyo.kind({
-	name: "App",
-	kind: "FittableRows",
-	fit: true,
-	components:[
-		{kind: "onyx.Toolbar", content: "Hello World"},
-		{kind: "enyo.Scroller", fit: true, components: [
-			{name: "main", classes: "nice-padding", allowHtml: true}
-		]},
-		{kind: "onyx.Toolbar", components: [
-			{kind: "onyx.Button", content: "Tap me", ontap: "helloWorldTap"}
-		]}
-	],
-	helloWorldTap: function(inSender, inEvent) {
-		this.$.main.addContent("The button was tapped.<br/>");
-	}
+  name: "App",
+  classes: "app enyo-fit",
+  components:[
+    { tag: "span", content: "Bluetooth Address: " },
+    { kind: "onyx.InputDecorator",
+      components: [
+        { name: "macAddress", kind: "onyx.Input", onchange: "addressChanged" }
+    ]},
+    { kind: "onyx.Button", content: "Connect", ontap: "connect" },
+    { kind: "onyx.Button", content: "Top",   cmd: "00100000 11010000", ontap: "sendCommand" },
+    { kind: "onyx.Button", content: "Down",  cmd: "00100000 11110000", ontap: "sendCommand" },
+    { kind: "onyx.Button", content: "Left",  cmd: "00010000 11100000", ontap: "sendCommand" },
+    { kind: "onyx.Button", content: "Right", cmd: "00110000 11100000", ontap: "sendCommand" },
+    { kind: "onyx.Button", content: "Stop",  cmd: "00100000 11100000", ontap: "sendCommand" },
+    { kind: "onyx.Button", content: "Show Device List", ontap: "showDeviceList" }
+  ],
+
+  rendered: function(){
+    this.inherited(arguments);
+    this.$.macAddress.setValue(localStorage.getItem("macAddress"));
+
+    bluetoothSerial.isEnabled(
+      null,
+      function(){
+        alert("Bluetooth is unable. Please turn on Bluetooth");
+    });
+  },
+
+  addressChanged: function( inSender, inEvent ){
+    localStorage.setItem("macAddress", this.$.macAddress.getValue());
+    return true;
+  },
+
+  connect: function( inSender, inEvent ){
+
+    var address = this.$.macAddress.getValue();
+    bluetoothSerial.isEnabled(
+      function(){
+        bluetoothSerial.connect(address);
+      },
+      function(){
+        alert("Bluetooth is unable. Please turn on Bluetooth");
+    });
+
+    return true;
+  },
+
+  sendCommand: function( inSender, inEvent ){
+
+    bluetoothSerial.isEnabled(
+      function(){
+        bluetoothSerial.write( inSender.cmd );
+      },
+      function(){
+        alert("Bluetooth is unable. Please turn on Bluetooth");
+    });
+
+    return true;
+  },
+
+  showDeviceList: function( inSender, inEvent ){
+
+    bluetoothSerial.isEnabled(
+      function(){
+        bluetoothSerial.list(
+          function( devices ){
+            var s = "Paired Bluetooth Device List:\n";
+            for( var i = 0; i < devices.length; i++ ){
+              s += (i+1) + ". " + devices[i].name + ": " + devices[i].address + "\n";
+            }
+            alert(s);
+        });
+      },
+      function(){
+        alert("Bluetooth is unable. Please turn on Bluetooth");
+    });
+
+    return true;
+  }
 });
